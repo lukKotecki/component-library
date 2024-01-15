f3b4a7f4a0d014b3dec727a2354605ff109f0ecd
Lukas <lukkotecki@gmail.com>
2024-01-15 21:30:52 +0100

    add files via CLI

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Badge from './components/Badge'
+
+export default function App(){
+  return (
+    <div className='app-container'>
+      <Badge />
+    
+    
+    </div>
+    
+  )
+}
+
---
+++ src/components/Badge.jsx
@@ -0,0 +1,10 @@
+import React  from 'react'
+
+export default function Badge(){
+    
+    return (
+        <div className='badge'>
+            Badge
+        </div>
+    )
+}
